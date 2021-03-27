ece87625058300868c8f0c6aef5fdc1acfc81c26
Aleksei Naumyshev <naumyshev@yandex.ru>
2021-03-27 09:36:56 +0300

    feature(295) EditText промежуточные правки

--- app/src/main/java/com/naumyshev/superapp/main/screens/edittext/EditTextFragment.kt
+++ app/src/main/java/com/naumyshev/superapp/main/screens/edittext/EditTextFragment.kt
@@ -3,9 +3,7 @@ package com.naumyshev.superapp.main.screens.edittext
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.naumyshev.superapp.R
 import com.naumyshev.superapp.databinding.FragmentEditTextBinding
@@ -13,6 +11,9 @@ import com.naumyshev.superapp.databinding.FragmentEditTextBinding
 class EditTextFragment: Fragment(R.layout.fragment_edit_text), EditTextContractInterface.View {
     companion object {
         const val TAG = "EditTextFragment"
+        const val STATE_FOCUS = "focus"
+        const val STATE_LOGIN = "login"
+        const val STATE_PASSWORD = "password"
     }
 
     private val editTextPresenter = EditTextPresenter()
@@ -21,7 +22,14 @@ class EditTextFragment: Fragment(R.layout.fragment_edit_text), EditTextContractI
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if (savedInstanceState != null) {
+            fragmentEditTextBinding?.focusTv?.text = savedInstanceState.getCharSequence(STATE_FOCUS)
+            fragmentEditTextBinding?.loginTv?.text = savedInstanceState.getCharSequence(STATE_LOGIN)
+            fragmentEditTextBinding?.passwordTv?.text = savedInstanceState.getCharSequence(STATE_PASSWORD)
+        }
+
         fragmentEditTextBinding = FragmentEditTextBinding.bind(view)
+        editTextPresenter.attach(this)
 
         fragmentEditTextBinding?.toolbar?.title = getString(R.string.edit_text)
         fragmentEditTextBinding?.toolbar?.setNavigationOnClickListener { activity?.onBackPressed() }
@@ -53,7 +61,34 @@ class EditTextFragment: Fragment(R.layout.fragment_edit_text), EditTextContractI
             override fun afterTextChanged(p0: Editable?) {
             }
         })
+        
+        fragmentEditTextBinding?.inputEt?.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                fragmentEditTextBinding?.focusTv?.text = getString(R.string.has_focus)
+            } else {
+                fragmentEditTextBinding?.focusTv?.text = getString(R.string.has_not_focus)
+            }
+        }
 
+        fragmentEditTextBinding?.inputEt?.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                fragmentEditTextBinding?.inputTv?.text = fragmentEditTextBinding?.inputEt?.editableText?.toString()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putCharSequence(STATE_FOCUS, fragmentEditTextBinding?.focusTv?.text)
+        outState.putCharSequence(STATE_LOGIN, fragmentEditTextBinding?.loginTv?.text)
+        outState.putCharSequence(STATE_PASSWORD, fragmentEditTextBinding?.passwordTv?.text)
     }
 
     override fun onDestroyView() {
@@ -62,6 +97,7 @@ class EditTextFragment: Fragment(R.layout.fragment_edit_text), EditTextContractI
     }
 
     override fun showLoginAndPass(){
-        fragmentEditTextBinding?.loginTv?.text = "тыщь"//fragmentEditTextBinding?.loginEt?.editableText?.toString()
+        fragmentEditTextBinding?.loginTv?.text = fragmentEditTextBinding?.loginEt?.editableText?.toString()
+        fragmentEditTextBinding?.passwordTv?.text = fragmentEditTextBinding?.passwordEt?.editableText?.toString()
     }
 }
